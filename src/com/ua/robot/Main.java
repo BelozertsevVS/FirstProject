876b0e9c393767aab66039f6cc2a2feb81643e6b
BelozertsevVS <bvs.wl1980@gmail.com>
2023-01-18 10:55:43 +0200

    HomeWork#2_Attempt_3

---
+++ src/com/ua/robot/Main.java
@@ -0,0 +1,9 @@
+package com.ua.robot;
+
+public class Main {
+
+    public static void main(String[] args) {
+        System.out.println("Hello Word !!! Я починаю курс Java в robot_dreams і вже вмію використовувати Git");
+    }
+
+}
