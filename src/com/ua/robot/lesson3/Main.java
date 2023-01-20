e72469c40245061104d00c526e3a636800276594
BelozertsevVS <bvs.wl1980@gmail.com>
2023-01-20 22:23:07 +0200

    HomeWork#3

---
+++ src/com/ua/robot/lesson3/Main.java
@@ -0,0 +1,18 @@
+package com.ua.robot.lesson3;
+
+public class Main {
+    public static void main(String[] args) {
+        int a = 10;
+        int b = 5;
+        System.out.println("The current value of the variable a = " + a);
+        System.out.println("The current value of the variable b = " + b);
+        System.out.println("a + b = ");
+        System.out.println(a + b);
+        System.out.println("a - b = ");
+        System.out.println(a - b);
+        System.out.println("a / b = ");
+        System.out.println(a / b);
+        System.out.println("a * b = ");
+        System.out.println(a * b);
+    }
+}
